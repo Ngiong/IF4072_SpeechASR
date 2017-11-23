d8f3d13527a1ae634c1e8ae77cdedc605758eed6
varian97 <variancaesar@gmail.com>
2017-11-23 18:21:45 +0700

    change file name in scp

--- All_Code/generate_scp.py
+++ All_Code/generate_scp.py
@@ -14,8 +14,14 @@ for file in files:
 			os.mkdir('./Dataset_MFCC/' + file + '/' + subfile)
 			subsubfiles = os.listdir('./Dataset/' + file + '/' + subfile)
 			for subsubfile in subsubfiles:
+				# rename the mfcc file
 				subsubfile_temp = subsubfile.replace(".wav", ".mfc")
-				result_range = './Dataset_MFCC' + '/' + file + '/' + subfile + '/' + subsubfile_temp
+				filename = subsubfile_temp[-8:-4]
+				num = ord(filename[0]) - 64
+				filename = filename.replace(filename[0], str(num)+'_')
+				print 'Processing %s into %s' % (subsubfile, filename)
+
+				result_range = './Dataset_MFCC' + '/' + file + '/' + subfile + '/' + filename
 				result_domain = './Dataset/' + file + '/' + subfile + '/' + subsubfile + ' ' + result_range
 				f.write(result_domain + '\n')
 f.close()
