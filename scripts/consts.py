34bd5626bd806c76f405fe70cbaccacd848bce36
varian97 <variancaesar@gmail.com>
2017-11-25 19:48:26 +0700

    hmm8 -hmm9 (step 8)

--- scripts/consts.py
+++ scripts/consts.py
@@ -16,9 +16,11 @@ MKPHONES1_FILE = "scripts/files/mlf/mkphones1.led"
 WORDS_MLF_FILE = "scripts/files/mlf/result/word.mlf"
 PHONES0_MLF_FILE = "scripts/files/mlf/result/phones0.mlf"
 PHONES1_MLF_FILE = "scripts/files/mlf/result/phones1.mlf"
+ALIGNED_MLF_FILE = "scripts/files/mlf/result/aligned.mlf"
 SILENCE_CONF_FILE = "scripts/files/silence_model/sil.hed"
 
 DLOG_FILE = "scripts/log/dlog"
+HVITE_LOG = "scripts/log/HVite_log"
 
 HMM_CONF_FILE = "scripts/files/hmm/hmm.conf"
 HMM_PROTO_FILE = "scripts/files/hmm/proto"
