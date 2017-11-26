958b0e51ad38daa2a50de5998325c43c55b8c40e
Muhammad Reza Ramadhan <rezaramadhan.m@gmail.com>
2017-11-26 12:23:59 +0700

    Fix list scp on crossvalidatioin

--- scripts/cross_validation.py
+++ scripts/cross_validation.py
@@ -22,32 +22,44 @@ def generate_transcript():
     if not os.path.exists("cross_validation"):
         os.mkdir("cross_validation")
 
+    # LIST FILE yang ada di directory
     file_list = {}
     for directory in DATA_DIR:
         file_list[directory] = []
         for subdir in os.listdir(ROOT_DIR + directory):
-             file_list[directory] += os.listdir(ROOT_DIR + directory + '/' + subdir)
+             file_list[directory] += sorted(os.listdir(ROOT_DIR + directory + '/' + subdir))
+            #  pprint(sorted(os.listdir(ROOT_DIR + directory + '/' + subdir)))
+            #  print(ROOT_DIR + directory + '/' + subdir)
+            #  pprint(directory + str(subdir))
 
+    # k = file_list.keys()
+    # pprint(sorted(file_list['set_1']))
+    # pprint(k)
     for directory in DATA_DIR:
         prompt_dir = "cross_validation/test_" + directory
 
         if not os.path.exists(prompt_dir):
             os.mkdir(prompt_dir)
-        pprint(file_list[directory]);
+        pprint(sorted(file_list[directory])[:5]);
         # with open(prompt_dir + '/prompts.tsv', 'w') as fout, open(prompt_dir + '/answer.tsv', 'w') as answer_out:
         #     for k in sorted(transcript.keys()):
         #         if k[2:] + '.mfc' not in file_list[directory]:
         #             fout.write(k + ' ' + transcript[k])
         #         else:
         #             answer_out.write(k + ' ' + transcript[k])
-        with open(MFCC_LIST_FILE, 'r') as fin:
-            mfc_list = fin.readlines()
-        gen_scp(file_list, directory, sorted(mfc_list))
+
+        data = open(MFCC_LIST_FILE, 'r').readlines()
+        gen_scp(file_list, directory, sorted(data))
 
 def gen_scp(file_list, directory, data):
-    scp_file = 'cross_validation/test_' + directory + '/mfcc_list.scp'
-    with open(scp_file, 'w') as fout:
+    print(directory)
+    train_scp_file = 'cross_validation/test_' + directory + '/train_list.scp'
+    test_scp_file = 'cross_validation/test_' + directory + '/answer_list.scp'
+
+    with open(train_scp_file, 'w') as answerfout, open(test_scp_file, 'w') as trainfout:
         for mfc in data:
-            if (mfc[26:len(mfc)-1] in file_list[directory]):
-                print("'"+mfc[26:len(mfc)-1]+"'")
-                fout.write(mfc)
+            if directory not in mfc:
+                # print("'"+mfc[26:len(mfc)-1]+"'")
+                answerfout.write(mfc)
+            else:
+                trainfout.write(mfc)
